41916a7b5b18f9ed8f3f4c28b62282f5d29358a0
27ycasper <27ycasper@go.dsdmail.net>
2024-12-16 15:54:17 +0000

    base program complete

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ oxen_price = 20
 rations_price = 2
 emergency_supplies_price = 5
 party_dead = False
+dead = 'rip'
+rand_num = ""
 
 
 
@@ -96,10 +98,14 @@ if health.upper() == 'A':
     if rations > 0:
         rations -= 1
     if days_until_winter <= 0:
-        party_health[0] += 1.5
-        party_health[1] += 1.5
-        party_health[2] += 1.5
-        party_health[3] += 1.5
+        if party_health[0] != dead:
+            party_health[0] += 1.5
+        if party_health[1] != dead:
+            party_health[1] += 1.5
+        if party_health[0] != dead:
+            party_health[2] += 1.5
+        if party_health[0] != dead:
+            party_health[3] += 1.5
     else:
         party_health[0] += 3
         party_health[1] += 3
@@ -107,11 +113,79 @@ if health.upper() == 'A':
         party_health[3] += 3
 else:
     print("You are not able to, sorry!")
-
-elif answer.upper() == 'B':
+if answer.upper() == 'B':
     if oxen > 0:
         days = random.randint(15,30)
-    else:
+        party_health[0] -= 3
+        party_health[1] -= 3
+        party_health[2] -= 3
+        party_health[3] -= 3
+        random_percent = random.randint(0,100)
+if random_percent <= 5:
+    oxen -= 1
+else:
         days = random.randint(30, 80)
+party_health[0] -= 6
+party_health[1] -= 6
+party_health[2] -= 6
+party_health[3] -= 6
 
-        
+miles = random.randint(10,40)
+miles_remaining -= miles
+days_until_winter -= days
+
+if answer.upper == 'C':
+    if oxen > 0:
+        days = random.randint(5,15)
+    party_health[0] -= 8
+    party_health[1] -= 8
+    party_health[2] -= 8
+    party_health[3] -= 8
+
+    miles = random.randint(30,60)
+    miles_remaining -= miles
+    days_until_winter -= days
+
+if answer.upper() == 'D':
+    print("Health of party: ")
+    print(party_health)
+
+print("Rations remaining: " + str(rations))
+
+answer = input("Are you sure you want to quit?: ")
+if "y" in answer.lower():
+    print("You have chosen to quit.")
+
+rand_num == random.randint(1,20)
+if rand_num == 1:
+
+    if emergency_supplies > 0: 
+        emergency_supplies -= 1
+
+else: 
+    party_health[0] -= 5
+    party_health[1] -= 5
+    party_health[2] -= 5  
+    party_health[3] -= 5
+    elif rand_num == 20:
+    party_health[0] += 5
+    party_health[1] += 5
+    party_health[2] += 5 
+    party_health[3] += 5
+else:
+
+if party_health[0] != dead and party_health[0] <= 0:
+    party_health[0] = dead
+if party_health[1] != dead and party_health[1] <= 0:
+    party_health[1] = dead
+
+if party_health.count(dead) == 4:
+    party_dead = True
+else: 
+    if not party_dead:
+print("Surviving members: ")
+if party_health[0] != dead:
+    print("/t" + party_members[0])
+if party_health[1] != dead:
+    print("/t" + party_members[1])
+else: 
